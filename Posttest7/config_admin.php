72687cb55c155f0e433c2047e5ebc85b5fd3132b
Muchlas Andrey <116377289+Leomuch1508@users.noreply.github.com>
2023-11-05 11:19:14 +0800

    Delete Posttest7 directory

--- Posttest7/config_admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $admins = [
-        ['email' => 'Muchlas@gmail.com', 'password' => password_hash('Muchlas1508', PASSWORD_DEFAULT)],
-        ['email' => 'Leomuch@gmail.com', 'password' => password_hash('Iyain', PASSWORD_DEFAULT)]
-    ];
-?>
--- Posttest7/function/tampil.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require "koneksi.php";
-
-    $result = mysqli_query($conn, "SELECT * FROM user");
-    $menu = [];
-    while ($row = mysqli_fetch_assoc($result)) {
-        $menu[] = $row;
-    }
-?>
